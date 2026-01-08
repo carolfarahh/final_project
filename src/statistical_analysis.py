38ae3f284693f7e817427f8a307cdad7cb847eff
amane <amaneegbaria18@gmail.com>
2026-01-08 21:03:53 +0200

    add Levene, Welch ANOVA, and Tukey functions

--- src/statistical_analysis.py
+++ src/statistical_analysis.py
@@ -9,3 +9,13 @@ def welch_anova(df, dependent_variable, independent_variable):
 def tukey(df, dependent_variable, independent_variable):
     results = pg.pairwise_tukey(data=df, dv=dependent_variable, between=independent_variable)
     return results
+
+import pingouin as pg
+
+def welch_anova(df, dependent_variable, independent_variable):
+    results = pg.welch_anova(data=df, dv=dependent_variable, between=independent_variable)
+    return results
+
+def tukey(df, dependent_variable, independent_variable):
+    results = pg.pairwise_tukey(data=df, dv=dependent_variable, between=independent_variable)
+    return results
