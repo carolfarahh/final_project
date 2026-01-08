5619d11b552e0aaf915d24f50f6b2a9fedc44228
Carol Farah <carolfarahfarah@gmail.com>
2026-01-08 14:54:16 +0200

    testing my patience

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ from tests.test_data_import import load_data_test
 import pandas as pd
 df = load_data("Huntington_Disease_Dataset.CSV")
 load_data_test(df)
-# print(df["Effect"].unique())
+print(df["Effect"].unique())
--- tests/test_data_import.py
+++ tests/test_data_import.py
@@ -4,14 +4,13 @@ import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
 from src.data_import import load_data
+
 def load_data_test(file_path):
     try:
-        load_data(file_path)
+        df = load_data(file_path)
     except FileNotFoundError:
         print(f"File not found: {file_path}")
     except PermissionError:
         print(f"Permission denied when accessing the file: {file_path}")
-    except Exception as e:
-        print("Failed to")
     else:
         print("Data set loaded successfully")
