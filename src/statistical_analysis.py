a2aad20f18f7e6c574b92fa8b98f158fd5f418a9
amane <amaneegbaria18@gmail.com>
2026-01-10 18:13:51 +0200

    add Levene test function

--- src/statistical_analysis.py
+++ src/statistical_analysis.py
@@ -12,10 +12,12 @@ def tukey(df, dependent_variable, independent_variable):
 
 import pingouin as pg
 
-def welch_anova(df, dependent_variable, independent_variable):
-    results = pg.welch_anova(data=df, dv=dependent_variable, between=independent_variable)
+def levene_test(df, dependent_variable, independent_variable):
+    results = pg.homoscedasticity(
+        data=df,
+        dv=dependent_variable,
+        group=independent_variable,
+        method="levene"
+    )
     return results
 
-def tukey(df, dependent_variable, independent_variable):
-    results = pg.pairwise_tukey(data=df, dv=dependent_variable, between=independent_variable)
-    return results
