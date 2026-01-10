8b7532ef39a50f1fbf0fb60c5bda50bba6ac4438
amane <amaneegbaria18@gmail.com>
2026-01-10 18:15:55 +0200

    add Levene test function

--- src/statistical_analysis.py
+++ src/statistical_analysis.py
@@ -10,7 +10,6 @@ def tukey(df, dependent_variable, independent_variable):
     results = pg.pairwise_tukey(data=df, dv=dependent_variable, between=independent_variable)
     return results
 
-import pingouin as pg
 
 def levene_test(df, dependent_variable, independent_variable):
     results = pg.homoscedasticity(
