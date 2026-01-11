2511d5ac048f1366d570d76c5a755e47bbbb89c5
amane <amaneegbaria18@gmail.com>
2026-01-11 04:10:40 +0200

    add tests for welch, tukey, and levene

---
+++ tests/test_data_analysis.py
@@ -0,0 +1,46 @@
+import pandas as pd
+from src.statistical_analysis import welch_anova
+
+
+def test_welch_anova():
+    df = pd.DataFrame({
+        "group": ["A", "A", "A", "B", "B", "B", "C", "C", "C"],
+        "y":     [1,   2,   1,   10,  11,  10,  5,   6,   5]
+    })
+
+    res = welch_anova(df, dependent_variable="y", independent_variable="group")
+
+    assert res is not None
+    assert isinstance(res, pd.DataFrame)
+    assert len(res) >= 1
+
+from src.statistical_analysis import tukey
+
+
+def test_tukey():
+    df = pd.DataFrame({
+        "group": ["A", "A", "A", "B", "B", "B", "C", "C", "C"],
+        "y":     [1,   2,   1,   10,  11,  10,  5,   6,   5]
+    })
+
+    res = tukey(df, "y", "group")
+
+    assert res is not None
+    assert isinstance(res, pd.DataFrame)
+    assert len(res) >= 1
+
+    
+from src.statistical_analysis import levene_test
+
+
+def test_levene_test():
+    df = pd.DataFrame({
+        "group": ["A", "A", "A", "B", "B", "B", "C", "C", "C"],
+        "y":     [1,   2,   1,   10,  11,  10,  5,   6,   5]
+    })
+
+    res = levene_test(df, "y", "group")
+
+    assert res is not None
+    assert isinstance(res, pd.DataFrame)
+    assert len(res) >= 1
